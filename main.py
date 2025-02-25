aa32d8ae7b6d7d18c528735fee924d7401ea723c
BOSS1480 <yboss1480@gmail.com>
2025-02-25 22:04:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from settings import settings_menu, handle_settings_input
 from database import init_db
 
 # החלף בטוקן האמיתי של הבוט שלך
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+TOKEN = "7523206004:AAEA0sANMSyGBYoGxAG1ixHO3wqW-Y7V4iE"
 
 def main():
     # אתחול מסד הנתונים
